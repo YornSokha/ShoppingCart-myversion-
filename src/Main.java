a275f778da78f5cdd6eae9bd3a43f432033c1e3b
SokhaYorn <yornsokha168@gmail.com>
2018-10-20 14:14:41 +0700

    move codes into main method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 
 public class Main {
 
-	System.out.println("Hello World");
-	System.out.println("Hello World 2");
-
-	System.out.println("Hello java project");
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World 2");
+		System.out.println("Hello java project");
+	}
 
 }
